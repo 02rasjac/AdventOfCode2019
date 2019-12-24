935a075866f3cbd9dbac1e72b7a15c7fc897699f
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2019-12-24 10:47:04 +0100

    Day 1 finished

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Day1
+{
+    class Program
+    {
+        static private int totalFuel = 0;
+
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader("Input.txt");
+            string filerow = "";
+
+            while ((filerow = sr.ReadLine()) != null)
+            {
+                int mass = int.Parse(filerow);
+                CalculateFuelReq(mass);
+            }
+
+            Console.WriteLine("Total Fuel: " + totalFuel);
+
+            Console.ReadLine();
+        }
+
+        static void CalculateFuelReq(int mass)
+        {
+            int fuel = 0;
+            fuel += (mass / 3) - 2;
+
+            if (fuel > 0)
+            {
+                totalFuel += fuel;
+
+                CalculateFuelReq(fuel);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Solutions for Advent of Code 2019
+
+This is my solutions for Advent of Code 2019 using C#.
