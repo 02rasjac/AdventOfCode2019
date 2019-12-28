fc70f5fc523f29e6d76cee8b6d9372ff9681f376
Rasmus Jacklin <rasmusjacklin2002@gmail.com>
2019-12-28 15:31:35 +0100

    Day 2 part 2

--- Day2/Day2/Program.cs
+++ Day2/Day2/Program.cs
@@ -28,8 +28,38 @@ namespace Day2
                 }
             }
             intcomp.Add(Convert.ToInt32(num));
+            List<int> originalList = new List<int>(intcomp);
 
-            // Loop through the list 
+            Console.WriteLine("Running...");
+
+            // Changes the noun and verb
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    // Sets the noun and verb
+                    ResetInput(ref intcomp, originalList);
+                    intcomp[1] = i;
+                    intcomp[2] = j;
+                    RunProgram(intcomp);
+
+                    // If it is the correct number, it should print the answer
+                    if (intcomp[0] == 19690720)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Noun=" + i);
+                        Console.WriteLine("Verb=" + j);
+                        Console.WriteLine("Answer: " + (100 * i + j));
+                        Console.ReadKey();
+                        Environment.Exit(0);
+                    }
+                }
+            }
+        }
+
+        static void RunProgram(List<int> intcomp)
+        {
+            // Do the calculations
             for (int i = 0; i < intcomp.Count; i++)
             {
                 // True if the first opcode = 99 and halts the program
@@ -38,25 +68,32 @@ namespace Day2
                 // If i is divisible by 4, than that means it is the start of an opcode
                 if ((i % 4) == 0)
                 {
-                    // Choses what should happen depending on the first opcode
-                    switch (intcomp[i])
+                    try
                     {
-                        case 1:
-                            int sum = intcomp[intcomp[i + 1]] + intcomp[intcomp[i + 2]];
-                            i += 3;
-                            intcomp[intcomp[i]] = sum;
-                            break;
-                        case 2:
-                            int factor = intcomp[intcomp[i + 1]] * intcomp[intcomp[i + 2]];
-                            i += 3;
-                            intcomp[intcomp[i]] = factor;
-                            break;
-                        case 99:
-                            halt = true;
-                            break;
-                        default:
-                            Console.WriteLine("***Something went wrong***");
-                            break;
+                        // Choses what should happen depending on the first opcode
+                        switch (intcomp[i])
+                        {
+                            case 1:
+                                int sum = intcomp[intcomp[i + 1]] + intcomp[intcomp[i + 2]];
+                                i += 3;
+                                intcomp[intcomp[i]] = sum;
+                                break;
+                            case 2:
+                                int factor = intcomp[intcomp[i + 1]] * intcomp[intcomp[i + 2]];
+                                i += 3;
+                                intcomp[intcomp[i]] = factor;
+                                break;
+                            case 99:
+                                halt = true;
+                                break;
+                            default:
+                                //Console.WriteLine("***Something went wrong***");
+                                break;
+                        }
+                    }
+                    catch
+                    {
+                        // Just exist to counter a crash
                     }
                 }
 
@@ -66,17 +103,11 @@ namespace Day2
                     break;
                 }
             }
+        }
 
-            // Prints the list
-            for (int i = 0; i < intcomp.Count; i++)
-            {
-                if (i == intcomp.Count - 1)
-                    Console.Write(intcomp[i]);
-                else
-                    Console.Write(intcomp[i] + ",");
-            }
-
-            Console.ReadKey();
+        static void ResetInput(ref List<int> resetList, List<int> originalList)
+        {
+            resetList = new List<int>(originalList);
         }
     }
 }
